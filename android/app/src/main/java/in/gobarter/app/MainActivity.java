bb1bebf1686ad464e006a0062f8f112be6dca801
Prashantpoddar7781 <Prashantpoddar29@gmail.com>
2026-08-04 16:05:15 +0530

    Add Capacitor Android wrapping and GitHub Actions cloud build workflow

---
+++ android/app/src/main/java/in/gobarter/app/MainActivity.java
@@ -0,0 +1,5 @@
+package in.gobarter.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.gobarter.app',
+  appName: 'BarterHub',
+  webDir: 'dist'
+};
+
+export default config;
